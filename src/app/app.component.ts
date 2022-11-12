1aaa422a2b5b102bb1701805d5eecd2f11d29226
marichemia <marichemia10@gmail.com>
2022-11-12 22:04:23 +0400

    commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+  <app-header></app-header>
+  <app-success-alert></app-success-alert>
+  <app-warning-alert></app-warning-alert>
+  <app-fail-alert></app-fail-alert>
+  <app-button></app-button>
+  <app-animation></app-animation>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
